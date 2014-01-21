5297718b1aac1427d572a9485021b327c32d6c38
iftekhar-ahmed <bit0220iftekhar@hotmail.com>
2014-01-22 02:44:33 +0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	bin/classes/com/example/booklocator/BookListActivity.class
    
    Signed-off-by: iftekhar-ahmed <bit0220iftekhar@hotmail.com>

--- gen/com/example/booklocator/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.booklocator;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/booklocator/adapter/BookAdapter.java
+++ src/com/booklocator/adapter/BookAdapter.java
@@ -9,8 +9,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.booklocator.activities.R;
 import com.booklocator.model.Book;
-import com.example.booklocator.R;
 
 public class BookAdapter extends BaseAdapter {
 
